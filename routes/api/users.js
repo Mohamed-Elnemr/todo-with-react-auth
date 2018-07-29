dd6163a4990191a5a9ec98fa6017f41b75a7fa92
Hussien El Attar <hussien.elattar1@gmail.com>
2018-07-29 04:56:50 +0200

    added login,register post routes and get current user, no val or midware

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,56 @@
 const express = require('express');
 const router = express.Router();
 
+// Load User model
+const User = require('../../models/User');
+
+// ******** NEEDS A MIDDLEWARE
+// @route   GET api/users/
+// @desc    Get Current User
+// @access  Private
+router.get('/', (req, res) => res.json({
+    id: req.user.id,
+    name: req.user.name,
+    password: req.user.password
+  })
+);
+
+// @route   Post api/users/register
+// @desc    register a new user
+// @access  Public
+router.post('/register', (req, res) => {
+  const newUserData = {
+    name: req.body.name,
+    password: req.body.password
+  }
+
+  const newUser = new User( newUserData )
+  newUser
+    .save()
+    .then(
+      user => res.json({ user })
+    )
+    .catch(
+      err => console.log(err)
+    )
+})
+
+// @route   Post api/users/login
+// @desc    login a user
+// @access  Public
+router.post('/login', (req, res) => {
+    const name = req.body.name
+    const password = req.body.password
+
+  User.findOne({name})
+    .then(user =>{
+      // Check for user
+      if (!user) {
+        return res.status(404).json('User not found');
+      }
+      
+      res.json({ user })
+    })
+    .catch(err => res.status(404).json("user is not found"))
+})
 module.exports = router;
