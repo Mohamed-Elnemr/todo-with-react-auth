da3990fdd2826e40bea9276fcc465a7a467c4285
Hussien El Attar <hussien.elattar1@gmail.com>
2018-07-30 08:03:09 +0200

    completed user register route with suitable validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const DataValidator = require('../../validation/dataValidator');
 
 // Load User model
@@ -21,31 +22,37 @@ router.get('/', (req, res) => res.json({
 // @access  Public
 router.post('/register', (req, res) => {
   const dataVal = new DataValidator();
-
-  const UserData = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    password2: req.body.password2
-  }
+  let errors = {};
   
-  if(!dataVal.isDataValid(UserData)){
+  if(!dataVal.isDataValid(req.body)){
     const errors = dataVal.errors;
     return res.status(400).json(errors);
   }
 
-  const newUser = new User(UserData)
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
+    };
 
-  newUser
-    .save()
-    .then(
-      user => res.json({ user })
-    )
-    .catch(
-      err => console.log(err)
-    )
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
 
-})
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err))
+      })
+    })
+  })
+});
 
 // @route   Post api/users/login
 // @desc    login a user
@@ -64,5 +71,6 @@ router.post('/login', (req, res) => {
       res.json({ user })
     })
     .catch(err => res.status(404).json("user is not found"))
-})
+});
+
 module.exports = router;
