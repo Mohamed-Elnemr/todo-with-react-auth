c907decf54b06e965d2f0267eefb06a02bab705d
Hussien El Attar <hussien.elattar1@gmail.com>
2018-07-30 23:30:14 +0200

    added todos (create, delete, show) routes

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -21,12 +21,82 @@ router.post("/", jwtMidware, (req,res)=>{
   newTodo = new Todo({
     text: req.body.text, owner: req.user._id
   })
-  
+
   newTodo
     .save()
     .then((todo)=>{
         res.json(todo)
     })
 })
+
+//______________________________________________________________________________________________________________
+// @route   GET api/profile
+// @desc    GET current Uesr todos
+// @access  Private
+router.get('/', jwtMidware,  (req, res) => {
+  let errors = {};
+
+  Todo.find({ owner: req.user._id })
+  .populate('owner', ['name'])
+  .then(todos => {
+     if (!todos) {
+       errors.todos = 'There is no todos for this user';
+       res.status(404).json(errors);
+     }
+
+     res.json(todos);
+  })
+  .catch((err)=>{res.status(404).json(err)});
+
+});
+
+//______________________________________________________________________________________________________________
+// @route   GET api/todos/owner/:user_id
+// @desc    Get todos by user id
+// @access  private
+router.get('/owner/:owner_id', jwtMidware, (req, res) => {
+  let errors = {};
+
+  // Check if the logged in user is the one making the request
+  if (req.params.owner_id != req.user._id) {
+    return res
+      .status(401)
+      .json({ notauthorized: 'User not authorized' });
+  } 
+
+  // Find user relative todos
+  Todo.find({ owner: req.params.owner_id })
+  .populate('owner', ['name'])
+  .then(todos => {
+     if (!todos) {
+       errors.todos = 'There is no todos for this user';
+       res.status(404).json(errors);
+     }
+
+     res.json(todos);
+  })
+  .catch(err =>
+     res.status(404).json({ todos: 'There are no todos for this user' })
+  );
+});
+
+//______________________________________________________________________________________________________________
+// @route   DELETE api/todos/:id
+// @desc    Delete todo
+// @access  Private
+router.delete('/:id', jwtMidware, (req, res) => {
+  Todo.findById(req.params.id)
+    .then(todo => {
+      // Check for todo owner
+      if (todo.owner.toString() !== req.user._id.toString()) {
+        return res
+          .status(401)
+          .json({ notauthorized: 'User not authorized' });
+      }
+      // Delete
+      todo.remove().then(() => res.json({ success: true }));
+    })
+    .catch(err => res.status(404).json({ todonotfound: 'No todo found' }))
+});
 
 module.exports = router;
