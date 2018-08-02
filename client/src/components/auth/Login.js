155d3091be9a34533924927fa1881a1dbb21ba43
Hussien El Attar <hussien.elattar1@gmail.com>
2018-08-02 04:19:20 +0200

    connected auth components with backend, they set JWT in local storage

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Login extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      errors: {},
+      isAuthen: this.props.isAuthen
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    // User data
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    axios
+      .post ( "/api/users/login", user)
+      .then( res => {
+        // Get token from the response then set it to localStorage
+        const token = res.data.token
+        localStorage.setItem('token', token);
+      })
+      .catch( err => 
+        this.setState({ errors: err.response.data })
+      );
+  }
+  
   render() {
     return (
       <div className="login">
@@ -15,13 +48,15 @@ class Login extends Component {
               <p className="lead text-center">
                 Sign in to your account
               </p>
-              <form noValidate >
+              <form noValidate onSubmit={this.onSubmit} >
                 <div className="form-group">
                   <input
                     type="email"
                     placeholder="Email Address"
                     name="email"
                     className='form-control form-control-lg'
+                    value={this.state.email}
+                    onChange={this.onChange} 
                   />
                 </div>
                 <div className="form-group">
@@ -30,6 +65,8 @@ class Login extends Component {
                     placeholder="Password"
                     name="password"
                     className='form-control form-control-lg'
+                    value={this.state.password}
+                    onChange={this.onChange} 
                   />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Register extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {},
+      isAuthen: this.props.isAuthen
+    };
+    this.onChange           = this.onChange.bind(this);
+    this.onSubmit           = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    axios
+    .post('/api/users/register', newUser)
+    .then( () =>
+        axios
+        .post ( "/api/users/login", newUser)
+        .then( res => {
+          // Get token from the response then set it to localStorage
+          const token = res.data.token
+          localStorage.setItem('token', token);
+        })
+        .catch( err => 
+          this.setState({ errors: err.response.data })
+        )
+    )
+    .catch( err => this.setState({ errors:err.response.data }))
   }
 
   render() {
@@ -15,13 +57,15 @@ class Register extends Component {
               <p className="lead text-center">
                 Create your account
               </p>
-              <form noValidate >
+              <form noValidate onSubmit={this.onSubmit} >
                 <div className="form-group">
                   <input
                     type="text"
                     placeholder="Name"
                     name="name"
                     className='form-control form-control-lg'
+                    value={this.state.name} 
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -30,6 +74,8 @@ class Register extends Component {
                     placeholder="Email Address"
                     name="email"
                     className='form-control form-control-lg'
+                    value={this.state.email} 
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -38,6 +84,8 @@ class Register extends Component {
                     placeholder="Password"
                     name="password"
                     className='form-control form-control-lg'
+                    value={this.state.password} 
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -46,6 +94,8 @@ class Register extends Component {
                     placeholder="Confirm Password"
                     name="password2"
                     className='form-control form-control-lg'
+                    value={this.state.password2} 
+                    onChange={this.onChange}
                   />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
